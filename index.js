1857980201411a05cc9ff8b7141a1bef9a7d1433
alaminjim <jimalamin7@gmail.com>
2025-04-02 12:56:21 +0800

    added update app get

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/equipments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await equipmentCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/equipment", async (req, res) => {
       const equipment = req.body;
       const result = await equipmentCollection.insertOne(equipment);
